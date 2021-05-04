daba56919ba371e681c23d2d847abb75cde7e436
Max Saperstone <msaperst@gmail.com>
2021-05-04 11:40:01 -0400

    Cleaning up imports

--- src/components/game/Game.space.test.jsx
+++ src/components/game/Game.space.test.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Game from './Game';
-// import { render } from "@testing-library/react";
 
 Enzyme.configure({ adapter: new Adapter() });
 
